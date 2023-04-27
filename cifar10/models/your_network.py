b50a734ac3e226c6838f3dd66901d46c1c54cb7d
Thomas Yuan <thomas@LAPTOP-LBE96NCJ.localdomain>
2023-04-27 16:14:15 +0100

    yes

---
+++ cifar10/models/your_network.py
@@ -0,0 +1,28 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class MyNetwork(nn.Module):
+    def __init__(self, optimizer):
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 6, 3)
+        self.bn1 = nn.BatchNorm2d(6)
+        self.conv2 = nn.Conv2d(6, 16, 3)
+        self.bn2 = nn.BatchNorm2d(16)
+        self.conv3 = nn.Conv2d(16, 32, 3)
+        self.bn3 = nn.BatchNorm2d(32)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.fc1 = nn.Linear(32 * 2 * 2, 84)
+        self.fc2 = nn.Linear(84, 10)
+        self.optimizer = optimizer
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.bn1(self.conv1(x))))
+        x = self.pool(F.relu(self.bn2(self.conv2(x))))
+        x = self.pool(F.relu(self.bn3(self.conv3(x))))
+        x = x.view(-1, 32 * 2 * 2)
+        x = torch.flatten(x, 1)  # flatten all dimensions except batch
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
