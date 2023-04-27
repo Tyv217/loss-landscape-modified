fc52a1c264fba9c9e48edfb4c88b79eab1da59b4
Thomas Yuan <thomas@LAPTOP-LBE96NCJ.localdomain>
2023-04-27 16:48:04 +0100

    yes

--- plot_trajectory.py
+++ plot_trajectory.py
@@ -80,5 +80,5 @@ if __name__ == '__main__':
                                 model_files1, args.dir_type, 'cos')
     proj_file2 = project_trajectory(dir_file2, w2, s2, args.dataset, args.model,
                                 model_files2, args.dir_type, 'cos')
-    plot_2D.plot_trajectory(proj_file1, dir_file1, savefig=False)
+    plot_2D.plot_trajectory(proj_file1, dir_file1, savefig=True)
     plot_2D.plot_trajectory(proj_file2, dir_file2)
